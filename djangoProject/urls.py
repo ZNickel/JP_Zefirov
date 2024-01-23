7b14de50a3a0eb92523b73e08c9e622f2c914733
ZNickel <Qwfsk@yandex.ru>
2024-01-23 16:43:40 +0500

    Added all pages blanks, mapping configured

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,25 +1,15 @@
-"""
-URL configuration for djangoProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 
-from main_app.views import main
+from main_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main),
+    path('', RedirectView.as_view(url='main/', permanent=False)),
+    path('main/', views.main, name='main'),
+    path('demand/', views.demand, name='demand'),
+    path('latest/', views.latest, name='latest'),
+    path('skills/', views.skills, name='skills'),
+    path('geography/', views.geography, name='geography'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 
 def main(request):
     return render(request, 'main_page.html')
+
+
+def demand(request):
+    return render(request, 'demand_page.html')
+
+
+def geography(request):
+    return render(request, 'geography_page.html')
+
+
+def skills(request):
+    return render(request, 'skills_page.html')
+
+
+def latest(request):
+    return render(request, 'latest_page.html')
