daf6a938b0ccec6d96792b0162a34e44906743fb
ZNickel <Qwfsk@yandex.ru>
2024-01-23 17:46:23 +0500

    Refactor DB structure

--- main_app/models.py
+++ main_app/models.py
@@ -1,22 +1,8 @@
 from django.db import models
 
 
-class Currency(models.Model):
-    name = models.CharField(max_length=50)
-    code = models.CharField(max_length=3)
-    conversion_rate_to_rub = models.DecimalField(max_digits=10, decimal_places=4)
-
-
-class Region(models.Model):
-    name = models.CharField(max_length=50)
-
-
 class Skill(models.Model):
     name = models.CharField(max_length=50)
-
-
-class Employer(models.Model):
-    name = models.CharField(max_length=100)
 
 
 class Vacancy(models.Model):
@@ -25,9 +11,9 @@ class Vacancy(models.Model):
     skills = models.ManyToManyField(Skill, through='VacancySkill')
     min_salary = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     max_salary = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    currency = models.ForeignKey(Currency, on_delete=models.CASCADE, null=True, blank=True)
-    employer = models.ForeignKey(Employer, on_delete=models.CASCADE, null=True, blank=True)
-    region = models.ForeignKey(Region, on_delete=models.CASCADE, null=True, blank=True)
+    currency_code = models.CharField(max_length=3, null=True, blank=True)
+    employer = models.CharField(max_length=100, null=True, blank=True)
+    region = models.CharField(max_length=50, null=True, blank=True)
     publication_date = models.DateField()
 
 
